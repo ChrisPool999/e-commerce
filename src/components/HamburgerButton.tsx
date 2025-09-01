f7a52139a2896df2b8195a56e1ee26905bb01966
Chris Pool <Brisbool999@gmail.com>
2025-09-01 03:43:40 -0700

    finished most of mobile nav btn and menu drag. to fix menu exit btn

--- src/components/HamburgerButton.tsx
+++ src/components/HamburgerButton.tsx
@@ -11,19 +11,19 @@ interface HamburgerButtonProps {
 }
 
 export default function HamburgerButton( {sizeClass = "h-8 w-8", extraClass = ''}: HamburgerButtonProps ) {
-  const {menuId, isOpen, openMenu, toggleMenu} = useMenu();
+  const {menuId, isOpen, openMenu} = useMenu();
 
   return ( 
     <button 
     type="button"
-    onClick={toggleMenu}
+    onClick={openMenu}
     className={`hamburger-btn flex relative ${sizeClass} cursor-pointer group ${extraClass} z-[100]`}
-    aria-label="menu button"
+    aria-label="open menu"
     aria-haspopup="menu"
     aria-expanded={isOpen}
     aria-controls={menuId}>
       
-      <div className="hamburger-btn__overlay absolute w-[180%] h-[180%] inset-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full opacity-30 group-hover:bg-gray-500 transition-all duration-300 ease-in-out"></div>
+      <div className="hamburger-btn__overlay absolute w-[180%] h-[180%] inset-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full opacity-30 group-hover:bg-gray-500  transition-all duration-300 ease-in-out"></div>
       
       <div className="hamburger-btn__bar-wrapper absolute flex inset-0 flex-col justify-between z-2">
         <span className="hamburger-btn__bar w-full h-1/5 bg-white"></span>
--- src/components/SlidingMenu.tsx
+++ src/components/SlidingMenu.tsx
@@ -3,26 +3,40 @@
 import { useMenu } from "@context/MenuContext";
 import React from "react";
 import { zIndex } from "@styles/zIndex";
+import ExitButton from "./ExitButton";
 
 interface SlidingMenuProps {
   IdName?: string;
   fromLeft?: boolean;
+  overlay?: boolean;
   screenWidth?: string;
   content?: React.ReactNode; 
 }
 
-export default function SlidingMenu( {content, fromLeft=true, screenWidth="70"}: SlidingMenuProps ) {
+export default function SlidingMenu( {content, fromLeft=true, overlay=true, screenWidth="40"}: SlidingMenuProps ) {
   const {menuId, isOpen, closeMenu} = useMenu();
 
   return (
-    <div 
-      id={menuId} 
-      className={`fixed top-0 left-0 h-full w-[80vw]  bg-gray-800 
-        transition-transform duration-500 ease-in-out z-[300]
-        ${isOpen ? "translate-x-0" : "-translate-x-full"}
-        `}
-    >
-      {content}
-    </div>
+    <nav>
+      <div 
+        style={{ width : `${screenWidth}vw` }}
+        id={menuId} 
+        className={`menu fixed top-0 left-0 h-full bg-gray-800 
+          transition-transform duration-500 ease-in-out z-[400]
+          ${isOpen ? "translate-x-0" : "-translate-x-full"}
+          `}
+      >
+        {content}
+      </div>
+      
+      {/* todo: make exit button dynamic with sliding window size */}
+      <ExitButton extraClass={`relative top-[20px] left-[calc(80vw+10px)] transiton-all ease-in-out duration-300 
+        ${!isOpen ? "opacity-0 pointer-events-none" : ""}`}  
+        />
+      
+      <div className={`overlay fixed inset-0 w-full h-full z-[10] bg-black transition-opacity duration-300 ease-in-out
+        ${isOpen ? "opacity-70" : "opacity-0"}`
+      }></div>
+    </nav>
   )
 }
