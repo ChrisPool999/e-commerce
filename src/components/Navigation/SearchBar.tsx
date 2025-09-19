8ae7e6708df7b174db512d018d046118c3ac9d09
Chris Pool <Brisbool999@gmail.com>
2025-09-19 15:31:59 -0700

    feat(search-bar): add search params

--- src/components/Navigation/SearchBar.tsx
+++ src/components/Navigation/SearchBar.tsx
@@ -1,12 +1,33 @@
+'use client'
+
+import { useState } from "react";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import SearchIcon from "@mui/icons-material/Search";
 
-export default function SearchBar() {
+export function SearchBar() {
+  const [query, setQuery] = useState("");
+
+  const searchParams = useSearchParams();
+  const params = new URLSearchParams(searchParams)
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  function handleSearch() {
+    if (query) {
+      params.set('search', query);
+    } else {
+      params.delete('search');
+    }
+    replace(`${pathname}?${params.toString()}`)
+  }
+
   return (
-    <form className="relative bg-white w-120 h-10 rounded-full flex justify-around z-[1000]">
+    <form onSubmit={handleSearch} className="relative bg-white w-120 h-10 rounded-full flex justify-around z-[1000]">
       <input
         type="text"
         placeholder="Search..."
         className="flex bg-transparent outline-none w-7/8 h-full text-center z-[1000]"
+        onChange={(e) => setQuery(e.target.value)}
       />
       <span className="w-0.5 h-full bg-black"></span>
       <button type="submit" className="cursor-pointer w-1/8 h-full rounded-r-full bg-gray-200">
